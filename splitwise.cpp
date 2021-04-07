de49c7c639810b2e2396b31943e08981c0aa8d31
abhi-jeet123 <47275248+abhi-jeet123@users.noreply.github.com>
2021-04-07 20:44:30 +0530

    Update splitwise.cpp

--- splitwise.cpp
+++ splitwise.cpp
@@ -1,11 +1,9 @@
 /*
-  Author- Abhijeet Trigunait 
+  Author- Abhijeet Trigunait
 */
 
 #include<bits/stdc++.h>
 #define lld long long int
-#define F first
-#define S second
 #define P pair<int,int>
 #define pb push_back
 #define mod 1e9+7
@@ -16,74 +14,74 @@
 
 using namespace std;
 
-class person_compare{
+class person_compare {
 public:
-	bool operator()(pair<string,int> p1,pair<string,int> p2){
-		return p1.second<p2.second;
+	bool operator()(pair<string, int> p1, pair<string, int> p2) {
+		return p1.second < p2.second;
 	}
 };
-int main(){
-	int no_of_transaction,friends;
-	cin>>no_of_transaction>>friends;
+int main() {
+	int no_of_transaction, friends;
+	cin >> no_of_transaction >> friends;
 
-	string x,y;
+	string x, y;
 	int amount;
 
-	map<string,int> net;
+	map<string, int> net;
 
-	while(no_of_transaction--){
-	    cin>>x>>y>>amount;
-		if(net.count(x)==0){
-			net[x]=0;
+	while (no_of_transaction--) {
+		cin >> x >> y >> amount;
+		if (net.count(x) == 0) {
+			net[x] = 0;
 		}
-		if(net.count(y)==0){
-			net[y]=0;
+		if (net.count(y) == 0) {
+			net[y] = 0;
 		}
-		net[x]-=amount;
-		net[y]+=amount;
+		net[x] -= amount;
+		net[y] += amount;
 	}
 
-    multiset<pair<string,int>,person_compare> m;
-    for(auto p:net){
-    	string person=p.first;
-    	int amount=p.second;
+	multiset<pair<string, int>, person_compare> m;
+	for (auto p : net) {
+		string person = p.first;
+		int amount = p.second;
 
-        if(net[person]!=0){
-        	m.insert(make_pair(person,amount));
-        }
+		if (net[person] != 0) {
+			m.insert(make_pair(person, amount));
+		}
 
-    }
-    int cnt=0;
-    //3.settlement amount and detail print
-    while(!m.empty()){
-    	auto low=m.begin();
-    	auto high=prev(m.end());
+	}
+	int cnt = 0;
+	//3.settlement amount and detail print
+	while (!m.empty()) {
+		auto low = m.begin();
+		auto high = prev(m.end());
 
-    	string debit_person=low->first;
-    	int debit=low->second;
+		string debit_person = low->first;
+		int debit = low->second;
 
-    	string credit_person=high->first;
-    	int credit=high->second;
-    	
-    	m.erase(low);
-    	m.erase(high);
+		string credit_person = high->first;
+		int credit = high->second;
 
-    	int settle_amount=min(-debit,credit);
+		m.erase(low);
+		m.erase(high);
 
-    	cout<<debit_person<<" will pay "<<settle_amount<<" to "<<credit_person<<endl;
+		int settle_amount = min(-debit, credit);
 
-    	debit+=settle_amount;
-    	credit-=settle_amount;
+		cout << debit_person << " will pay " << settle_amount << " to " << credit_person << endl;
 
-    	if(debit!=0){
-    		m.insert(make_pair(debit_person,debit));
-    	}
-    	if(credit!=0){
-    		m.insert(make_pair(credit_person,credit));
-    	}
-    	cnt+=1;
-    }
-    cout<<"Total no of transaction "<<cnt<<endl;
+		debit += settle_amount;
+		credit -= settle_amount;
+
+		if (debit != 0) {
+			m.insert(make_pair(debit_person, debit));
+		}
+		if (credit != 0) {
+			m.insert(make_pair(credit_person, credit));
+		}
+		cnt += 1;
+	}
+	cout << "Total no of transaction " << cnt << endl;
 
 
 
